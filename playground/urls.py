24937ecee3218a939a024c483a73a1c0efcc88b0
Md.Ashraful Momen <amshuvo64@gmail.com>
2023-09-28 22:02:17 +0600

    smtp mail complete

--- playground/urls.py
+++ playground/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from playground import views
-# app_name = 'playground'
+app_name = 'playground'
 urlpatterns = [
-    path('hello/',views.say_hello, name='hello')
+    path('hello/',views.say_hello, name='hello'),
+    path('sendMain/',views.sendMain,name='sendMain')
 ]
--- storefront/settings.py
+++ storefront/settings.py
@@ -183,3 +183,15 @@ REST_FRAMEWORK = {
 }
 
 
+#fake smtp configure: --------------------------------
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL = 'from@shuvo.com'
+
+ADMINS = [
+    ('Shuvo', 'kali@Shuvo.com')
+]
